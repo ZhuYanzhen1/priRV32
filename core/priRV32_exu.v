0d57885d2b0af16520252dd959ba2e39684bd6f5
Lao·Zhu <2208213223@qq.com>
2022-02-14 20:53:53 +0800

    Add the address calculation of access instruction and access address output interface in EXU.

--- core/priRV32_exu.v
+++ core/priRV32_exu.v
@@ -3,6 +3,9 @@ module priRV32_EXU(
     input rst_n,
     output reg[31:0] mem_readwrite_address,
     input [31:0] mem_read_data,
+    output reg[31:0] mem_write_data,
+    output reg [4:0] rd_reg_latched,
+    output reg[31:0] reg_write_data,
     input [31:0] imm_decoded,
     input [31:0] rs1_decoded,
     input [31:0] rs2_decoded,
@@ -34,7 +37,7 @@ module priRV32_EXU(
     assign is_jalr_addi_slti_sltiu_xori_ori_andi = |{instr_jalr, instr_addi, instr_slti, instr_sltiu, instr_xori, instr_ori, instr_andi};
     assign is_sb_sh_sw = |{instr_sb, instr_sh, instr_sw};
 
-    reg [31:0] alu_out, alu_add_sub, alu_shl, alu_shr, reg_op1, reg_op2, reg_out;
+    reg [31:0] alu_out, alu_add_sub, alu_shl, alu_shr, reg_op1, reg_op2, reg_out, mem_out;
     reg alu_eq, alu_ltu, alu_lts;
     always @* begin
         mem_readwrite_address <= rs1_decoded + imm_decoded;
@@ -49,7 +52,7 @@ module priRV32_EXU(
         alu_out <= 'bx;
         case (1'b1)
 			|{instr_lui, instr_auipc, instr_jal, instr_jalr, instr_addi, instr_add, instr_sub}:
-				alu_out <= alu_add_sub;
+				alu_out <= instr_jalr ? alu_add_sub + 32'd4 : alu_add_sub;
 			|{is_beq_bne_blt_bge_bltu_bgeu, instr_slti, instr_slt, instr_sltiu, instr_sltu}: begin
                 case (1'b1)
                     instr_beq:
@@ -100,14 +103,39 @@ module priRV32_EXU(
             mem_readwrite_address <= alu_add_sub;
         else
             mem_readwrite_address <= 32'b0;
+    end
+
+    always @(*) begin
         case (1'b1)
+            |{instr_lui, instr_auipc, instr_jal, instr_jalr, instr_addi, instr_add, instr_sub
+            , instr_xori, instr_xor, instr_ori, instr_or, instr_andi, instr_and, instr_sll, instr_slli
+            , instr_srl, instr_srli, instr_sra, instr_srai}:
+                reg_out <= alu_out;
             instr_lb:
                 reg_out <= $signed(mem_read_data[7:0]);
-            |{instr_lbu, instr_lhu, instr_lw}: 
+            |{instr_lbu, instr_lhu, instr_lw}:
                 reg_out <= mem_read_data;
 			instr_lh:
                 reg_out <= $signed(mem_read_data[15:0]);
+            instr_sw:
+                mem_out <= rs2_decoded;
+            instr_sh:
+                mem_out <= {16'h0000, rs2_decoded[15:0]};
+            instr_sb:
+                mem_out <= {24'h000000, rs2_decoded[7:0]};
         endcase
     end
 
+    always @(posedge clk_i) begin
+        if(!{instr_sw, instr_sh, instr_sb}) begin
+            mem_write_data <= mem_out;
+
+            reg_write_data <= 32'h00000000;
+            rd_reg_latched <= 5'd0;
+        end else begin
+            reg_write_data <= reg_out;
+            rd_reg_latched <= rd_reg;
+        end
+    end
+    
 endmodule
